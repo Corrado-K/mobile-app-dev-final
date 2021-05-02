e925dd5ad6f41750d502d80765a87db856bd5a70
Maxmillianmart01 <65747828+Maxmillianmart01@users.noreply.github.com>
2021-05-02 17:54:22 +0000

    CHANGES

--- voterX/src/app/home/home.page.ts
+++ voterX/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private splashScreen: SplashScreen) {
+    this.splashScreen.show();
+
+    this.splashScreen.hide();
+  }
 
 }
